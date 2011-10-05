3cfefa748b5d3ac48ba879c18ca0739c0705ae98
Febri DAmatraseta Fairuz <febryfairuz@gmail.com>
2011-10-05 18:25:15 +0700

    soal 10

---
+++ Season 5/Soal 10/Praktikum.java
@@ -0,0 +1,13 @@
+public class Praktikum{
+	public static void main(String[] args){
+	//menginisialisasi variabel num dengan tipe data integer dan memiliki nilai 11
+		int num=11;
+		//apakah 11 lebih besar dari 5
+		if(num > 5)
+			//jika benar ,aka mencetak keluaran word "Number is Greater Than 5"
+			System.out.println("Number is Greater Than 5");
+		else
+			//jika benar ,aka mencetak keluaran word "Number is less Than 5"
+			System.out.println("Number is less Than 5");
+	}
+}
