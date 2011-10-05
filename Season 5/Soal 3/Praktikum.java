219897739163c4e10fe420cb144cfa37b44e7ec3
Febri DAmatraseta Fairuz <febryfairuz@gmail.com>
2011-10-05 18:23:20 +0700

    soal 3

---
+++ Season 5/Soal 3/Praktikum.java
@@ -0,0 +1,10 @@
+public class Praktikum{
+	public static void main(String args[]){
+		//melakukan control flow while dengan isi parameter tipe data boolean true
+		while(true) {
+		//mencetak keluaran berupa words "Hello, Worlds we are using loops"
+		System.out.println("Hello, Worlds we are using loops");
+		}
+		//jadi melakukan terus pengulangan tiada henti karena boolean terus meminta true
+	}
+}
