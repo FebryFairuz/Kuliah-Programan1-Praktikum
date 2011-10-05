7658977518016acc0e52ffa463c4e536952c5c69
Febri DAmatraseta Fairuz <febryfairuz@gmail.com>
2011-10-05 18:24:32 +0700

    soal 7

---
+++ Season 5/Soal 7/Praktikum.java
@@ -0,0 +1,9 @@
+public class Praktikum{
+	public static void main(String[] args){
+	//menginisialisasi variabel age dengan tipe data integer dan memiliki nilai 21
+		int age = 21;
+		//melakukan pemilihan apakah 21 lebih besar dari 50 
+		//jika benar, maka mencetak keluaran "You are young"
+		System.out.println(age > 50 ? "You are old" : "You are young");
+	}
+}
