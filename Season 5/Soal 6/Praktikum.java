ac5541b439a9ad3a7d3da438d182fcee0a051c17
Febri DAmatraseta Fairuz <febryfairuz@gmail.com>
2011-10-05 18:24:13 +0700

    soal 6

---
+++ Season 5/Soal 6/Praktikum.java
@@ -0,0 +1,14 @@
+public class Praktikum{
+	public static void main(String args[]){
+	//inisialisasi variabel coba1 dengan tipe data boolean
+		boolean coba1; //--> boolean hanya berisi true or false
+		//memberikan nilai coba1 apakah 10 sama dengan 1 ?
+		coba1 = (10 == 1);
+		//mencetak keluaran dari hasil nilai coba1 yaitu true
+			System.out.println(coba1);
+		//memberikan nilai coba1 apakah 10 tidak sama dengan 1 ?	
+		coba1 = (10 != 1);
+		//mencetak keluaran dari hasil nilai coba1 yaitu false
+			System.out.println(coba1);
+	}
+}
