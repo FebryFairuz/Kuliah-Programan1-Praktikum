dd7efe5b3c429a39c9bd344e45094bf380466a65
Febri DAmatraseta Fairuz <febryfairuz@gmail.com>
2011-10-05 18:23:04 +0700

    soal 2

---
+++ Season 5/Soal 2/Praktikum.java
@@ -0,0 +1,14 @@
+public class Praktikum{
+		public static void main(String args[]){
+		//melakukan perLoopingan dengan For
+			for (int x = 0; x < 7; x++)
+			//inisialisasi variabel x dengan tipe data integer
+			//memberi nilai 0 pada variabel x
+			//jika x lebih kecil dari 7 
+			{
+			//maka mencetak keluaran berupa words "Hello, Worlds we are using loops"
+			System.out.println("Hello, Worlds we are using loops");
+			//x + 1
+			}
+		}
+}
