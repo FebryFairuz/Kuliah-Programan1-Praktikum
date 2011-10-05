375590bb7911506f7de9509a6a4bff0a83f94a3c
Febri DAmatraseta Fairuz <febryfairuz@gmail.com>
2011-10-05 18:23:38 +0700

    soal 4

---
+++ Season 5/Soal 4/Praktikum.java
@@ -0,0 +1,15 @@
+public class Praktikum{
+	public static void main(String[] args){
+	//mencetak keluaran word "Instr 1"
+	System.out.println("Instr 1");
+		//jika 3 lebih besar dari 2 maka mencetak keluaran word "Instr 2"
+		if(3>2){
+			System.out.println("Instr 2");
+		}else{
+		//jika salah maka mencetak keluaran word "Instr 3"
+			System.out.println("Instr 3");
+		}
+		//mencetak keluaran word "Instr 4"
+		System.out.println("Instr 4");
+	}
+}
