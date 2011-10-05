919bd488fca79f35afa1a21638d29da3b596bcae
Febri DAmatraseta Fairuz <febryfairuz@gmail.com>
2011-10-05 18:24:45 +0700

    soal 8

---
+++ Season 5/Soal 8/Praktikum.java
@@ -0,0 +1,19 @@
+public class Praktikum{
+	public static void main(String args[]){
+	//menginisilisasi variabel count dengan tipe data integer dan memiliki nilai 1
+		int count = 1;
+		//melakukan control flow while 
+		//apakah 1 lebih kecil dari 5 
+		//apakah 2 lebih kecil dari 5 
+		//dan seterusnya sampai apakah 5 lebih besar dari 5
+		while(count < 5){
+			//count=1
+			//count=2
+			//dan seterusnya
+			//jika benar maka mencetak keluaran Count is equal + count
+			System.out.println("Count is equal to " + count);
+			//count + 1
+			count++;
+		}//jika salah maka selesai melakukan perulangan
+	}
+}
