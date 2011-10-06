da5c43bf25593aecda49da0d0556df510e77fb44
Febri DAmatraseta Fairuz <febryfairuz@gmail.com>
2011-10-06 09:11:37 +0700

    soal 9 baru

--- Season 5/Soal 9/Praktikum.java
+++ Season 5/Soal 9/Praktikum.java
@@ -1,14 +1,27 @@
 public class Praktikum{
 	public static void main(String[] args){
+	//menginisialisasi variabel a dan b dengan tipe data integer
+	//memberikan nilai pada variabel a = 4 dan b = 6
 		int a=4,b=6;
+		//inisialisasi variabel num dengan tipe data integer
+		//memberikan nilai pada variabel num = a or b
 		int num=a|b;
+		//jika num sama dengan 4 maka
 		if(num==4)
+			//mencetak "value is 4"
 			System.out.println("value is 4");
+		//jika num sama dengan 5 maka
 		else if(num==5)
+			//mencetak "value is 5"
 			System.out.println("value is 5");
+		//jika num sama dengan 6 maka
 		else if(num==6)
+			//mencetak "value is 6"
 			System.out.println("value is 6");
+		//jika num sama dengan 7
 		else if(num==7)
+			//maka mencetak "value is 7"
 			System.out.println("value is 7");
 	}
+	//jika tidak maka selesai
 }
